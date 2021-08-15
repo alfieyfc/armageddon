451d25d77099eda7c51038c16f78c1becd5ac041
alfieyfc <alfieyfc@github.com>
2021-08-16 07:26:39 +0800

    workspace setup

---
+++ src/main/java/info/alfieyfc/armageddon/Armageddon.java
@@ -0,0 +1,11 @@
+package info.alfieyfc.armageddon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Armageddon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
